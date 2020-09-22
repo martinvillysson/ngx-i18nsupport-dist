826652b31c46ce1dfdda10589ff6ba78340a9b21
Martin Villyson <martin.villyson@sigma.se>
2020-09-22 11:18:58 +0200

    remove

--- tooling/lib/tooling.module.d.ts
+++
@@ -1,5 +0,0 @@
-import * as i0 from "@angular/core";
-export declare class ToolingModule {
-    static ɵmod: i0.ɵɵNgModuleDefWithMeta<ToolingModule, never, never, never>;
-    static ɵinj: i0.ɵɵInjectorDef<ToolingModule>;
-}
